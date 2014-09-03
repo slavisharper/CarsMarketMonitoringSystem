2ec75d51d9315f8370876805d8bdb1fc3f343340
kalimar <kalimar@abv.bg>
2014-09-03 20:05:21 +0300

    Adding missing xml parser

---
+++ CarsMarketMonitoringSystem.Data/Xml/Parsers/XmlParser.cs
@@ -0,0 +1,68 @@
+﻿
+namespace CarsMarketMonitoringSystem.Data.Xml
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+    using System.Xml.Schema;
+    using System.Xml;
+    using System.IO;
+    using Models;
+
+    public static class XmlParser
+    {
+        const string SalesSchemaPath = @"Xml\Schemas\ExpensesSchema.xsd";
+
+        public static IEnumerable<Expense> LoadExpensesData(string filePath)
+        {
+            var salesXml = XElement.Load(filePath);
+            ValidateXml(salesXml, SalesSchemaPath);
+
+            var expenseTags = salesXml.Descendants("Expense");
+            List<Expense> expensesParsed = new List<Expense>(expenseTags.Count());
+
+            foreach (var expense in expenseTags)
+            {
+                var expenceId = GetTagValueOrNull(expense, "ExpenseId");
+                var manufacturerId = GetTagValueOrNull(expense, "ManufacturerId");
+                var expenses = GetTagValueOrNull(expense, "ExpenseValue");
+                var month = GetTagValueOrNull(expense, "Month");
+
+                expensesParsed.Add(new Expense()
+                {
+                    ExpenceId = int.Parse(expenceId),
+                    ManufacturerId = int.Parse(manufacturerId),
+                    Expences = decimal.Parse(expenses),
+                    Month = DateTime.Parse(month)
+                });
+            }
+
+            return expensesParsed;
+        }
+
+        private static void ValidateXml(XElement xmlData, string schemaFile)
+        {
+            XmlTextReader reader = new XmlTextReader(schemaFile);
+            XmlSchema schema = XmlSchema.Read(reader,
+                (sender, args) => { throw new ArgumentException("Could not read schema", args.Exception); });
+
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add(schema);
+
+            XDocument document = new XDocument(xmlData);
+            document.Validate(schemas, (sender, args) =>
+                {
+                    throw new ArgumentException("The supplied XElement is not in the valid format", args.Exception);
+                }, true);
+        }
+
+        private static string GetTagValueOrNull(XElement xml, string tagName, bool isRequired = true)
+        {
+            var tag = isRequired ? xml.Descendants(tagName).Single() :
+                xml.Descendants(tagName).FirstOrDefault();
+
+            return tag != null ? tag.Value : null; 
+        }
+    }
+}
