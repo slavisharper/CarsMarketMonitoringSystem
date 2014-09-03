3f628c2f80270c2a537b7e1c90a6a20b8df64f43
Slavi <slavicvetanov@gmail.com>
2014-09-03 12:03:02 +0300

    PDf saver class added

---
+++ CarsMarketMonitoringSystem.Data/PdfReporter/PdfReporter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsMarketMonitoringSystem.Data.PdfReporter
+{
+    public class PdfReporter
+    {
+        private DataManager manager;
+        public PdfReporter(DataManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
